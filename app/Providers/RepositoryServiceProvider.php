01f93c4cf088c3a9b2e242ea6f1e062d7fd36c70
Maxuel Rodrigues <maxuel.rodrigues@gmail.com>
2022-05-19 10:14:06 -0300

    creating repository service and register

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\BaseRepository;
+use App\Repositories\Contracts\EloquentRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
